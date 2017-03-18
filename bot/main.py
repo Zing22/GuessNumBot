e9420943a9c0b1b420eaf383e1119a49bb1a5a02
Zing22 <leezing22@gmail.com>
2017-03-18 16:21:18 +0800

    接收任意好友请求

--- bot/main.py
+++ bot/main.py
@@ -30,11 +30,11 @@ def raw_msg_handle(msg):
 # 自动接受验证信息中包含 'wxpy' 的好友请求
 def auto_accept_friends(msg):
     # 判断好友请求中的验证文本
-    if ACCEPT_KEYWORD in msg.text.lower():
-        # 接受好友 (msg.card 为该请求的用户对象)
-        new_friend = bot.accept_friend(msg.card)
-        # 或 new_friend = msg.card.accept()
-        # 向新的好友发送消息
-        new_friend.send(ACCEPT_GREETING)
+    # if ACCEPT_KEYWORD in msg.text.lower():
+    # 接受好友 (msg.card 为该请求的用户对象)
+    new_friend = bot.accept_friend(msg.card)
+    # 或 new_friend = msg.card.accept()
+    # 向新的好友发送消息
+    new_friend.send(ACCEPT_GREETING)
 
 bot.start()
